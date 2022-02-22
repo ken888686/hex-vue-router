f23296fb66ff5e3819c7553517d14c2cbfbffee8
Aaron <ken888686@gmail.com>
2022-02-22 16:34:08 +0800

    get customer products

--- src/views/Client/Products.vue
+++ src/views/Client/Products.vue
@@ -1,17 +1,111 @@
 <template>
-  <div>
-    {{ title }}
+  <div class="container">
+    <table class="table align-middle">
+      <thead>
+        <tr>
+          <th>圖片</th>
+          <th>商品名稱</th>
+          <th>價格</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(item, index) in products"
+          :key="index"
+        >
+          <td style="width: 200px">
+            <div
+              style="
+                height: 100px;
+                background-size: cover;
+                background-position: center;
+              "
+            >
+              <img
+                :src="item.imageUrl"
+                style="width: 100%; height: 100%; object-fit: cover"
+              >
+            </div>
+          </td>
+          <td>{{ item.title }}</td>
+          <td>
+            <del class="h6">原價 {{ item.origin_price }} 元</del>
+            <div class="h5">
+              現在只要 {{ item.price }} 元
+            </div>
+          </td>
+          <td>
+            <div class="btn-group btn-group-sm">
+              <button
+                type="button"
+                class="btn btn-outline-secondary"
+                :disabled="isLoading && loadingItemId === item.id"
+                @click="showProductInfoModal(item.id)"
+              >
+                <i
+                  v-if="isLoading && loadingItemId === item.id"
+                  class="fas fa-spinner fa-pulse"
+                />
+                查看更多
+              </button>
+              <button
+                type="button"
+                class="btn btn-outline-danger"
+                :disabled="isLoading && loadingItemId === item.id"
+                @click="addProduct({ productId: item.id, quantity: 1 })"
+              >
+                <i
+                  v-if="isLoading && loadingItemId === item.id"
+                  class="fas fa-spinner fa-pulse"
+                />
+                加到購物車
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
+import { customer } from '@/service';
+
 export default {
   data() {
     return {
-      title: 'Products',
+      products: [],
+      isLoading: false,
+      loadingItemId: '',
+      pagination: {
+        current_page: 1,
+        has_next: false,
+        has_pre: false,
+        total_pages: 1,
+      },
     };
   },
   mounted() {
-    console.log('Products');
+    this.getProducts();
+  },
+  methods: {
+    getProducts() {
+      const loader = this.$loading.show();
+      customer
+        .getProducts()
+        .then((res) => {
+          console.log(res.data);
+          const { products, pagination } = res.data;
+          this.products = products;
+          this.pagination = pagination;
+        })
+        .catch((err) => {
+          console.dir(err);
+        })
+        .finally(() => {
+          loader.hide();
+        });
+    },
   },
 };
 </script>
